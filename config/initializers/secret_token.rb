993ddab5ed7d0e21cbf77acfc352afda4d2cd4da
surrealdetective <thefifth@gmail.com>
2013-08-25 11:51:46 -0400

    adds artist model to have associations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Query::Application.config.secret_token = 'ca5665ce350a5bae6097bc794d2bdd264486d4dce2b768a9dc3eff88638bfd556e34b3587594bf4e67e19d3c582bad9cc64337da947b4a03da89f7e16473f9d9'
